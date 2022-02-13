7f84576c3c96b30040897588e6cc5560610d9018
ArsenyChistyakov <samson200289@mail.ru>
2022-02-13 13:55:33 +0300

    update and install pockets; added goods.json (products list for product)

--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.conf import settings
 from django.shortcuts import render
 
 
@@ -7,20 +10,10 @@ def index(request):
 
 
 def products(request):
+    file_path = settings.BASE_DIR /'products/fixtures/goods.json'
+
     context = {
         'title': 'geekShop - Продукты',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090,
-             'text': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
-            {'name': 'Синяя куртка The North Face', 'price': 23725,
-             'text': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390,
-             'text': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
-            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340, 'text': 'Плотная ткань. Легкий материал.'},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590,
-             'text': 'Гладкий кожаный верх. Натуральный материал.'},
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890,
-             'text': 'Легкая эластичная ткань сирсакер Фактурная ткань.'},
-        ],
+        'products': json.load(open(file_path, encoding='utf-8'))
     }
     return render(request, 'products/products.html', context)
