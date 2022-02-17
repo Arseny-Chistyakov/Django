acfacc5b5ac9b79522b5c238784eafab54d92136
ArsenyChistyakov <samson200289@mail.ru>
2022-02-17 13:12:34 +0300

    added single quote style

--- products/views.py
+++ products/views.py
@@ -4,14 +4,14 @@ from products.models import ProductCategory, Product
 
 
 def index(request):
-    context = {'title': 'geekShop'}
-    return render(request, 'products/index.html', context=context)
+    context = {"title": "geekShop"}
+    return render(request, "products/index.html", context=context)
 
 
 def products(request):
     context = {
-        'title': 'GeekShop - Каталог',
-        'productcategory': ProductCategory.objects.all(),
-        'products': Product.objects.all()
+        "title": "GeekShop - Каталог",
+        "productcategory": ProductCategory.objects.all(),
+        "products": Product.objects.all()
     }
-    return render(request, 'products/products.html', context=context)
+    return render(request, "products/products.html", context=context)
