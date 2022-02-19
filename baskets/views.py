ffdb643ee3365195d36b14960ab94b8410bf6b11
ArsenyChistyakov <samson200289@mail.ru>
2022-02-19 16:58:28 +0300

    added basket_remove() method

--- baskets/views.py
+++ baskets/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import HttpResponseRedirect
 
-from products.models import Product
 from baskets.models import Basket
+from products.models import Product
 
 
 def basket_add(request, product_id):
@@ -17,3 +17,9 @@ def basket_add(request, product_id):
         basket.quantity += 1
         basket.save()
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_remove(request, id):
+    basket = Basket.objects.get(id=id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
