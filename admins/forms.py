0699353bf260f8ddd839a3c330414f7920212bf4
ArsenyChistyakov <samson200289@mail.ru>
2022-02-27 15:17:23 +0300

    added create controller(admin-users-create)

---
+++ admins/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from users.forms import UserRegistrationForm
+from users.models import User
+
+
+class UserAdminRegistrationForm(UserRegistrationForm):
+    image = forms.ImageField(widget=forms.FileInput(attrs={"class": "custom-file-input"}), required=False)
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "image", "username", "email", "password1", "password2", "bot_check")
+
+    # another example validator
+    # def clean_username(self):
+    #     username = self.cleaned_data["username"]
+    #     if User.objects.filter(username=username).exists():
+    #         raise ValueError
--- admins/views.py
+++ admins/views.py
@@ -1,11 +1,31 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
 
 
 def index(request):
     context = {'title': 'GeekShop - Admin'}
     return render(request, 'admins/index.html', context)
+
+
+# create controller
+def admin_users_create(request):
+    if request.method == "POST":
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            messages.success(request, "Congratulation! Success create new user")
+            form.save()
+            return HttpResponseRedirect(reverse("admins_special:admin_users"))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'title': 'GeekShop - Admin', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
 
 
 # read controller
