51c5c448a53080d24278156d8fe2936eabda951a
ArsenyChistyakov <samson200289@mail.ru>
2022-02-27 20:36:52 +0300

    added CRUD for products

--- admins/forms.py
+++ admins/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-from users.forms import UserRegistrationForm, UserProfileForm
+from products.models import Product, ProductCategory
+from users.forms import UserRegistrationForm, UserProfileForm, UserChangeForm
 from users.models import User
 
 
@@ -23,3 +24,16 @@ class UserAdminProfileForm(UserProfileForm):
     email = forms.CharField(widget=forms.EmailInput(attrs={"class": "form-control py-4"}))
     bot_check = forms.CharField(
         widget=forms.TextInput(attrs={"class": "form-control py-4", "placeholder": "Введите HyperPop"}))
+
+
+class ProductAdminProfileForm(UserChangeForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4"}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={"class": "custom-file-input"}), required=False)
+    description = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4"}))
+    price = forms.DecimalField(widget=forms.TextInput(attrs={"class": "form-control py-4"}))
+    quantity = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4"}))
+    category = forms.ModelChoiceField(queryset=ProductCategory.objects.all())
+
+    class Meta:
+        model = Product
+        fields = ("name", "image", "description", "price", "quantity", "category")
--- admins/views.py
+++ admins/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm, ProductAdminProfileForm
+from products.models import Product
 from users.models import User
 
 
@@ -32,12 +33,37 @@ def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html', context)
 
 
+# create controller
+@user_passes_test(lambda u: u.is_staff)
+def admin_products_create(request):
+    if request.method == "POST":
+        form = ProductAdminProfileForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            messages.success(request, "Congratulation! Success create new item")
+            form.save()
+            return HttpResponseRedirect(reverse("admins_special:admin_products"))
+        else:
+            print(form.errors)
+    else:
+        form = ProductAdminProfileForm()
+    context = {'title': 'GeekShop - Admin', 'form': form}
+    return render(request, 'admins/admin-products-create.html', context)
+
+
 # read controller
 @user_passes_test(lambda u: u.is_staff)
 def admin_users(request):
     users = User.objects.all()
     context = {'title': 'GeekShop - Admin', 'users': users}
     return render(request, 'admins/admin-users-read.html', context)
+
+
+# read controller
+@user_passes_test(lambda u: u.is_staff)
+def admin_products(request):
+    products = Product.objects.all()
+    context = {'title': 'GeekShop - Admin', 'products': products}
+    return render(request, 'admins/admin-products-read.html', context)
 
 
 # update controller
@@ -58,6 +84,24 @@ def admin_users_update(request, pk):
     return render(request, 'admins/admin-users-update-delete.html', context)
 
 
+# update controller
+@user_passes_test(lambda u: u.is_staff)
+def admin_products_update(request, pk):
+    selected_product = Product.objects.get(id=pk)
+    if request.method == "POST":
+        form = ProductAdminProfileForm(instance=selected_product, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            messages.success(request, "Data has been saved ")
+            form.save()
+            return HttpResponseRedirect(reverse("admins_special:admin_products"))
+        else:
+            print(form.errors)
+    else:
+        form = ProductAdminProfileForm(instance=selected_product)
+    context = {'title': 'GeekShop - Admin', 'form': form, 'selected_product': selected_product}
+    return render(request, 'admins/admin-products-update-delete.html', context)
+
+
 # delete controller
 @user_passes_test(lambda u: u.is_staff)
 def admin_users_delete(request, pk):
@@ -65,3 +109,12 @@ def admin_users_delete(request, pk):
     user.save_deleted()
     messages.success(request, f"{user.username} has been deactivated ")
     return HttpResponseRedirect(reverse('admins_special:admin_users'))
+
+
+# delete controller
+@user_passes_test(lambda u: u.is_staff)
+def admin_products_delete(request, pk):
+    product = Product.objects.get(id=pk)
+    product.delete()
+    messages.success(request, f"{product.name} has been deleted ")
+    return HttpResponseRedirect(reverse('admins_special:admin_products'))
