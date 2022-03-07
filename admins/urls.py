99200bc76019072d76791997cc0a73c4ca821e6a
ArsenyChistyakov <samson200289@mail.ru>
2022-03-07 11:33:22 +0300

    added UserAdminUpdateView,ProductAdminUpdateView,UserAdminDeleteView,ProductAdminDeleteView

--- admins/urls.py
+++ admins/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from admins.views import index, UserAdminListView, UserAdminCreateView, admin_users_update, admin_users_delete, \
-    ProductAdminListView, ProductAdminCreateView, admin_products_update, admin_products_delete
+from admins.views import index, UserAdminListView, UserAdminCreateView, UserAdminUpdateView, UserAdminDeleteView, \
+    ProductAdminListView, ProductAdminCreateView, ProductAdminUpdateView, ProductAdminDeleteView
 
 app_name = "admins"
 
@@ -9,11 +9,11 @@ urlpatterns = [
     path("", index, name="index"),
     path("users/", UserAdminListView.as_view(), name="admin_users"),
     path("users-create/", UserAdminCreateView.as_view(), name="admin_users_create"),
-    path("users-update/<int:pk>/", admin_users_update, name="admin_users_update"),
-    path("users-delete/<int:pk>/", admin_users_delete, name="admin_users_delete"),
+    path("users-update/<int:pk>/", UserAdminUpdateView.as_view(), name="admin_users_update"),
+    path("users-delete/<int:pk>/", UserAdminDeleteView.as_view(), name="admin_users_delete"),
 
     path("products/", ProductAdminListView.as_view(), name="admin_products"),
     path("products-create/", ProductAdminCreateView.as_view(), name="admin_products_create"),
-    path("products-update/<int:pk>/", admin_products_update, name="admin_products_update"),
-    path("products-delete/<int:pk>/", admin_products_delete, name="admin_products_delete"),
+    path("products-update/<int:pk>/", ProductAdminUpdateView.as_view(), name="admin_products_update"),
+    path("products-delete/<int:pk>/", ProductAdminDeleteView.as_view(), name="admin_products_delete"),
 ]
